733f99c4967fb0fd76be09e46984187a7d6c1933
Douglas Winter <132670815+DGzzzzz@users.noreply.github.com>
2024-03-16 14:28:15 -0300

    ajustes nas funcionalidades

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,39 @@
 import { useState } from "react";
 
 import Perfil from "./components/Perfil";
-import Formulario from "./components/Formulario";
 import ReposList from "./components/RepoList";
 
-
+import './global.css';
 
 
 function App() {
-  const [formularioEstaVisivel, setFormularioVisivel] = useState(true);
   const [nomeUsuario, setNomeUsuario] = useState('');
   return (
     <>
-    
+      {nomeUsuario.length <= 4 && (
+        <>
+          <Perfil nomeUsuario={nomeUsuario}/>
+          <input 
+            type="text" 
+            onBlur={(e) => setNomeUsuario(e.target.value)} 
+            placeholder="Digite o nome do usuário"
+          />
+        </>
+      )}
 
-    {nomeUsuario.length > 4 && (
-      <>
-        <Perfil nomeUsuario={nomeUsuario}/>
-        <input type="text" onBlur={(e) => setNomeUsuario(e.target.value)} />
-        <ReposList nomeUsuario={nomeUsuario}/>
-      </>
-    )}
-
-    
-    {/* {formularioEstaVisivel && (
-      <Formulario />
-    )}
-    <button onClick={() => setFormularioVisivel(!formularioEstaVisivel)} type="button">toggle form</button> */}
-    </>
-  )
+      {nomeUsuario.length > 4 && (
+        <>
+          <Perfil nomeUsuario={nomeUsuario}/>
+          <input 
+            type="text" 
+            onBlur={(e) => setNomeUsuario(e.target.value)} 
+            placeholder="Digite o nome do usuário"
+          />
+          <ReposList nomeUsuario={nomeUsuario}/>
+        </>
+      )}
+    </>  
+  );
 }
 
 export default App
